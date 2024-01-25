9254c61a7cd219f23d5ef6fd2f92338d3683871c
TLii <tlii@iki.fi>
2024-01-25 09:58:00 +0200

    fix: Add root checks to entrypoint

--- fs/usr/local/bin/docker-entrypoint.sh
+++ fs/usr/local/bin/docker-entrypoint.sh
@@ -1,23 +1,47 @@
 #!/usr/bin/env bash
 
+if [ "$EUID" -ne 0 ]
+  then IAMROOT=false
+else
+  IAMROOT=true
+fi
+
   # Run user's setup.sh
   if [[ -f $PWD/setup.sh ]]; then
-    chmod +x "$PWD/setup.sh"
+    if [[ ! $IAMROOT ]]; then
+      sudo chmod +x "$PWD/setup.sh"
+    else
+      chmod +x "$PWD/setup.sh"
+    fi
     "$PWD/start.sh"
   fi
 
   # Run additional entrypoint scripts.
   if [[ -d /usr/local/bin/entrypoint.d ]]; then
-    chmod +x /usr/local/bin/entrypoint.d/*.sh
+    if [[ ! $IAMROOT ]]; then
+      sudo chmod +x /usr/local/bin/entrypoint.d/*.sh
+    else
+      chmod +x /usr/local/bin/entrypoint.d/*.sh
+    fi
+
     for f in /usr/local/bin/entrypoint.d/*.sh; do
       echo "Running $f"
-      sudo "$f"
+      if [[ ! $IAMROOT ]]; then
+        sudo "$f"
+      else
+        "$f"
+      fi
     done
   fi
 
   # Run any custom startup scripts in userspace
   if [[ -f "$PWD/start.sh" ]]; then
-    chmod +x "$PWD/start.sh"
+    if [[ ! $IAMROOT ]]; then
+      sudo chmod +x "$PWD/start.sh"
+    else
+      chmod +x "$PWD/start.sh"
+    fi
+
     "$PWD/start.sh"
   fi
 
