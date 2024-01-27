8ad5243e4bbc507dee11478fd27048c6ce65d900
TLii <tlii@iki.fi>
2024-01-27 22:01:57 +0200

    fix: Remove irrelevant chmod commands

--- fs/usr/local/bin/docker-entrypoint.sh
+++ fs/usr/local/bin/docker-entrypoint.sh
@@ -8,21 +8,11 @@ fi
 
   # Run user's setup.sh
   if [[ -f $PWD/setup.sh ]]; then
-    if [[ ! $IAMROOT ]]; then
-      sudo chmod +x "$PWD/setup.sh"
-    else
-      chmod +x "$PWD/setup.sh"
-    fi
     "$PWD/start.sh"
   fi
 
   # Run additional entrypoint scripts.
   if [[ -d /usr/local/bin/entrypoint.d ]]; then
-    if [[ ! $IAMROOT ]]; then
-      sudo chmod +x /usr/local/bin/entrypoint.d/*.sh
-    else
-      chmod +x /usr/local/bin/entrypoint.d/*.sh
-    fi
 
     for f in /usr/local/bin/entrypoint.d/*.sh; do
       echo "Running $f"
@@ -36,12 +26,6 @@ fi
 
   # Run any custom startup scripts in userspace
   if [[ -f "$PWD/start.sh" ]]; then
-    if [[ ! $IAMROOT ]]; then
-      sudo chmod +x "$PWD/start.sh"
-    else
-      chmod +x "$PWD/start.sh"
-    fi
-
     "$PWD/start.sh"
   fi
 
